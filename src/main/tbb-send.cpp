2ec0417cde0a6cdc63e5d0b471941d6415596d94
wamos <kelvinstwang@gmail.com>
2018-02-08 14:21:50 -0800

    tbb send-recv pair, with connection issues

--- src/main/tbb-send.cpp
+++ src/main/tbb-send.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <inttypes.h>
 #include "tbb/tbb_allocator.h"
 #include "tbb/flow_graph.h"
 #include "tbb/concurrent_queue.h"
 #include "tbb/tick_count.h"
+#include "Socket.h"
 
 using namespace std;
 using namespace tbb;
@@ -14,7 +18,7 @@ struct TupleBuffer {
 	uint64_t _capacity;
 	uint64_t _size;
 
-	TupleBuffer(uint64_t capacity = (1024 * 1024 * 16))
+	TupleBuffer(uint64_t capacity = (1024 * 1024 * 1))
 		: _capacity(capacity), _size(capacity) {
 		_data = new uint8_t[_capacity];
 		memset(_data, 0, _capacity);
@@ -25,17 +29,25 @@ struct TupleBuffer {
 			delete [] _data;
 		}
 	}
+	
+	/*bool markLast(){
+   		capacity=0;
+	}*/
+	
+	uint8_t* getBuffer(){
+		return _data;
+	}
 
 	inline uint64_t size() const { return _size; }
 	inline uint64_t capacity() const { return _capacity; }
 };
 
-struct MySource {
+struct Sourcer {
 	const int LIMIT = 10000;
 	int count;
 
-	MySource() {
-		count = 0;
+	Sourcer() {
+		count = 0;		
 	}
 
 	bool operator()(TupleBuffer *&v) {
@@ -46,6 +58,43 @@ struct MySource {
 		} else {
 			cout << "Source emitted " << LIMIT << " buffers" << endl;
 			return false;
+		}
+	}
+};
+
+struct Sender {
+	const int LIMIT = 10000;
+	int count;
+	uint64_t sent_bytes;
+	std::string ipaddr= "10.1.100.31"; // server
+    std::string port="8000";
+    uint64_t Retry=100;
+    uint64_t retryDelayInMicros=1000;
+	Socket* send_socket;
+
+	Sender() {
+		count = 0;	
+		sent_bytes = 0;
+		Socket* send_socket=new Socket();	
+		send_socket->tcpConnect(ipaddr, port, 1024*1024, retryDelayInMicros, Retry);
+	}
+
+	/*	
+	~Sender(){
+		send_socket->tcpClose();
+	}*/
+
+	TupleBuffer * operator()(TupleBuffer *v) {
+		if (count < LIMIT) {
+			//send_socket->tcpConnect(ipaddr, port, 1024*1024, retryDelayInMicros, Retry);
+			++count;
+			uint64_t num_bytes = send_socket->tcpSend(v->getBuffer(), v->size());
+			sent_bytes = sent_bytes + num_bytes;
+			return v;
+		} else {
+			send_socket->tcpClose();
+			cout << "send " << sent_bytes << " bytes" << endl;
+			return v;
 		}
 	}
 };
@@ -75,12 +124,12 @@ int main(int argc, char* argv[])
 	graph producer_side;
 	//graph consumer_side;
 
-	source_node<TupleBuffer*> src(producer_side, MySource(), /* is_active */ false );
-	function_node<TupleBuffer*,TupleBuffer*> func1(producer_side, unlimited, Worker_NOP());
-	function_node<TupleBuffer*> sink(producer_side, unlimited, PrintIt());
+	source_node<TupleBuffer*> src(producer_side, Sourcer(), /* is_active */ false );
+	//function_node<TupleBuffer*, TupleBuffer*> func1(producer_side, unlimited, Worker_NOP());
+	function_node<TupleBuffer*, TupleBuffer*> send(producer_side, 1 , Sender());
 
-	make_edge(src, func1);
-	make_edge(func1, sink);
+	make_edge(src, send);
+	//make_edge(func1, send);
 
 	//source_node<TupleBuffer*> src2(consumer_side, MySource(), /* is_active */ false );
 	//function_node<TupleBuffer*,TupleBuffer*> func2(consumer_side, unlimited, Worker_NOP());
